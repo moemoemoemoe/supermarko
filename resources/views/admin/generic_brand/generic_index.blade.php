eac5f9d1b5a0b0c7e8ea2a7d97b33b8791bef1ce
mohamad hamieh <mohammad-hamieh@hotmail.com>
2018-02-02 09:44:40 +0200

    schedulesaver

--- resources/views/admin/generic_brand/generic_index.blade.php
+++ resources/views/admin/generic_brand/generic_index.blade.php
@@ -53,7 +53,7 @@
 
                 <div class="panel-body">
                                                                                
-@foreach($generics as $generics)
+@foreach($generics as $generic)
     <div class="col-md-3">
         <div class="panel panel-default">
             <div class="panel-heading text-center">
